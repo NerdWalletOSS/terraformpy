ab9215bd7c7b4d396dd30ef2711dec6fe1d565e4
Borgstrom ♕ <evan@borgstrom.ca>
2017-01-27 10:34:47 -0800

    Rebuild CLI to not depend on click, and drop .main() (#7)

--- setup.py
+++ setup.py
@@ -8,10 +8,6 @@ setup(
     version=VERSION,
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[
-        'click==6.6',
-        'sh==1.11',
-    ],
     entry_points={
         'console_scripts': [
             'terraformpy = terraformpy.cli:main',
--- src/terraformpy/cli.py
+++ src/terraformpy/cli.py
@@ -1,51 +1,36 @@
 import imp
 import json
-import logging
 import os
 import sys
 
-import click
-
 from terraformpy import compile
 
-log = logging.getLogger(__name__)
 
-
-@click.command()
-@click.option('-D', '--debug', is_flag=True)
-@click.argument('terraform_args', nargs=-1, type=click.UNPROCESSED)
-def main(debug, terraform_args):
+def main():
     """Compile *.tf.py files and run Terraform"""
-    if debug:
-        level = logging.DEBUG
-    else:
-        level = logging.INFO
-    logging.basicConfig(level=level)
-
     to_process = [
         ent
         for ent in os.listdir(os.getcwd())
         if ent.endswith('.tf.py')
     ]
+
     if len(to_process) == 0:
-        log.error("Error loading config: No Terraformpy (.tf.py) files found in %s", os.getcwd())
+        print("terraformpy - Error loading config: No .tf.py files found in %s" % os.getcwd())
         sys.exit(1)
 
-    log.info("terraformpy - processing: %s", to_process)
+    print("terraformpy - Processing: %s" % ', '.join(to_process))
 
     # all we need to do is import our files
     # the nature of resource declaration will register all of the objects for us to compile
     for filename in to_process:
-        tfpy = imp.load_source(filename[:-6], filename)
+        imp.load_source(filename[:-6], filename)
 
-        # run the .main() function on the module, ignore it if the module hasn't defined one
-        try:
-            tfpy.main()
-        except AttributeError:
-            pass
-
+    # now 'compile' everything that was registered, and write it out the tf.json file
+    print("terraformpy - Writing main.tf.json")
     with open('main.tf.json', 'w') as fd:
         json.dump(compile(), fd, indent=4, sort_keys=True)
 
-    # replace ourself with terraform
-    os.execvp("terraform", ["terraform"] + list(terraform_args))
+    if len(sys.argv) > 1:
+        print("terraformpy - Running terraform: %s" % ' '.join(sys.argv[1:]))
+        # replace ourself with terraform
+        os.execvp("terraform", ["terraform"] + sys.argv[1:])
