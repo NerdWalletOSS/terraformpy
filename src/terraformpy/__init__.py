1dd4fb50c0e6021046bbe16997c563b8d9094518
Wyatt Peterson <wpeterson@nerdwallet.com>
2017-04-11 15:44:32 -0700

    Add a DuplicateKey object to be used by Provider. This allows multiple providers of the same type to be defined, such as multiple "mysql" providers.

--- src/terraformpy/__init__.py
+++ src/terraformpy/__init__.py
@@ -1,5 +1,5 @@
 from .resource_collections import ResourceCollection, Input, Variant  # noqa
-from .objects import TFObject, Provider, Variable, Data, Resource, Output  # noqa 
+from .objects import TFObject, Provider, Variable, Data, Resource, Output, DuplicateKey  # noqa 
 
 # add a couple shortcuts
 compile = TFObject.compile
