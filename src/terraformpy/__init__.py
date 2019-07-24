bc93d93214351b9d6cf3642660760ee340c80c2c
Travis Beatty <tbeatty@nerdwallet.com>
2019-07-24 14:27:10 -0400

    Added ordered dict for schematic usage (#39)

--- src/terraformpy/__init__.py
+++ src/terraformpy/__init__.py
@@ -1,4 +1,4 @@
-from .objects import Data, DuplicateKey, Output, Provider, Resource, Terraform, TFObject, Variable  # noqa
+from .objects import Data, DuplicateKey, OrderedDict, Output, Provider, Resource, Terraform, TFObject, Variable  # noqa
 from .resource_collections import ResourceCollection, Variant  # noqa
 
 # add a couple shortcuts
