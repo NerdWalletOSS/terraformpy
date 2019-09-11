b6fc37d507cfadd7cdc277e44c897681c205a588
Jordan Winters <jwinters@nerdwallet.com>
2019-09-11 10:38:50 -0500

    nit updates

--- setup.py
+++ setup.py
@@ -15,7 +15,10 @@ class VerifyVersionCommand(install):
     description = 'verify that the git tag matches our version'
 
     def run(self):
-        tag = os.getenv('CIRCLE_TAG')
+        if os.getenv('CIRCLE_TAG') == None:
+            sys.exit("FAILED: Your CIRCLE_TAG variable appears to be unset.  Are you running on CircleCI triggered by tag?")
+        else:
+            tag = os.getenv('CIRCLE_TAG')
 
         if tag != "v" + VERSION:
             info = "Git tag: {0} does not match the version of this app: {1}".format(
